a1d6692504d9bb4c1da96c4a203b7ae89cafdc0b
Alonzo Machiraju <machthree@gmail.com>
2020-12-20 16:21:00 -0800

    basic setup with navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import logo from './Images/huskerbeach-192.png';
 import './App.css';
+
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<Navbar bg="dark" variant="dark">
+<Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <NavDropdown title="Middleware" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Unity</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Unreal</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#home">Games</Nav.Link>
+      <Nav.Link href="#home">Support</Nav.Link>
+      <Nav.Link href="#home">About</Nav.Link>
+    </Nav>
+    </Navbar.Collapse>
+    <Navbar.Brand href="#home">
+      <img
+        src={logo}
+        height="96"
+        width="96"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+    </Navbar.Brand> 
+  </Navbar>
+      <div>
+      Put a router here for further info
+      </div>
     </div>
   );
 }
