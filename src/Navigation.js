ad90dbb2d5ac08f85a49728fa1473cec16c2ed65
Alonzo Machiraju <machthree@gmail.com>
2021-01-02 20:12:45 -0800

    fixed alt text for logo

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,33 +1,33 @@
-import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 import logo from "./Images/huskerbeach-192.png";
 
 export default function Navigation() {
-    return(
-        <navbar>
-            <Navbar collapseOnSelect expand="lg" fixed="top" bg="dark" variant="dark">
-            <Navbar.Brand href="/">
+  return (
+    <navbar>
+      <Navbar collapseOnSelect expand="lg" fixed="top" bg="dark" variant="dark">
+        <Navbar.Brand href="/">
           <img
             src={logo}
             height="96"
             width="96"
             className="d-inline-block align-top"
-            alt="React Bootstrap logo"
+            alt="HuskerBeach logo"
           />
-        </Navbar.Brand> 
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <NavDropdown title="Middleware" id="basic-nav-dropdown">
-            <NavDropdown.Item href="middleware">Unity</NavDropdown.Item>
-            <NavDropdown.Item href="/middleware">Unreal</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link href="/games">Games</Nav.Link>
-          <Nav.Link href="/support">Support</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
-        </Nav>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavDropdown title="Middleware" id="basic-nav-dropdown">
+              <NavDropdown.Item href="middleware">Unity</NavDropdown.Item>
+              <NavDropdown.Item href="/middleware">Unreal</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="/games">Games</Nav.Link>
+            <Nav.Link href="/support">Support</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Navbar>
-        </navbar>
-    )
+    </navbar>
+  );
 }
