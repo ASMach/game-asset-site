a0a069c2c3ea9cfe1caabf3a5982573a1d4aa21f
Alonzo Machiraju <machthree@gmail.com>
2021-01-02 20:12:31 -0800

    image for first unity module

--- src/Pages/Middleware.js
+++ src/Pages/Middleware.js
@@ -1,31 +1,49 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import {Container, Tab, Tabs} from 'react-bootstrap';
+import { Container, Tab, Tabs, Row, Image } from "react-bootstrap";
 
-import * as eventKeys from '../tabKeys';
+import * as eventKeys from "../tabKeys";
 
-export default function Middleware (props) {
+import predictive_character_seeking_unity_sm from "../Images/Unity/predictive-character-seeking-sm.jpg";
+import predictive_character_seeking_unity_lg from "../Images/Unity/predictive-character-seeking-lg.jpg";
 
-    const [key, setKey] = useState('home');
+export default function Middleware(props) {
+  const [key, setKey] = useState("home");
 
-    return (
-        <div className="App-body">
-            <div className="about">
-                <Container>
-                <Tabs
-      id="controlled-tab-example"
-      activeKey={key}
-      onSelect={(k) => setKey(k)}
-    >
-      <Tab eventKey={eventKeys.UNITY} title="Unity">
-        Predictive Character Seeking
-      </Tab>
-      <Tab eventKey={eventKeys.UNREAL} title="Unreal">
-        Unreal middleware
-      </Tab>
-    </Tabs>
-                </Container>
-            </div>
-        </div>
-    );
+  return (
+    <div className="App-body">
+      <div className="about">
+        <Container>
+          <Tabs
+            id="controlled-tab-example"
+            activeKey={key}
+            onSelect={(k) => setKey(k)}
+          >
+            <Tab eventKey={eventKeys.UNITY} title="Unity">
+              <Container>
+                <Row>
+                  <Image
+                    src={predictive_character_seeking_unity_sm}
+                    className="d-inline-block align-top"
+                    alt="Predictive Character Seeking"
+                    rounded
+                  />
+                </Row>
+                <Row>
+                  <p>
+                    Real-time linear regression to predict where a character
+                    will be once beyond an AI character's range of sight. A new
+                    angle to NPC seeking scripts.
+                  </p>
+                </Row>
+              </Container>
+            </Tab>
+            <Tab eventKey={eventKeys.UNREAL} title="Unreal">
+              Unreal middleware
+            </Tab>
+          </Tabs>
+        </Container>
+      </div>
+    </div>
+  );
 }
