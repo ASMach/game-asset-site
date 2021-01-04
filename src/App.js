4600ad966402bc3f73590b15440b2bc2c486e88d
Alonzo Machiraju <machthree@gmail.com>
2021-01-04 12:11:48 -0800

    basic scaffolding for ReCAPTCHA

--- src/App.js
+++ src/App.js
@@ -1,40 +1,57 @@
-import './App.css';
+import "./App.css";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Navigation from './Navigation';
+import { loadReCaptcha } from "react-recaptcha-google";
+
+import Navigation from "./Navigation";
 import Footer from "./Footer";
 
-import Home from './Pages/Home';
-import Middleware from './Pages/Middleware';
-import Games from './Pages/Games';
-import Support from './Pages/Support';
-import About from './Pages/About';
+import Home from "./Pages/Home";
+import Middleware from "./Pages/Middleware";
+import Games from "./Pages/Games";
+import Support from "./Pages/Support";
+import About from "./Pages/About";
 
-import * as eventKeys from './tabKeys';
+import * as eventKeys from "./tabKeys";
 
-function App() {
-  return (
-    <div className="App">
-<Router>
-        <Navigation />
-        < br />
-           < br />
-           < br />
-           < br />
-           < br />
-           < br />
-        <Switch>
-          <Route path="/" exact component={() => <Home />} />
-          <Route path="/middleware" exact component={() => <Middleware key={eventKeys.UNREAL}/>} />
-          <Route path="/games" exact component={() => <Games />} />
-          <Route path="/support" exact component={() => <Support />} />
-          <Route path="/about" exact component={() => <About />} />
-        </Switch>
-        <Footer />
-      </Router>
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    loadReCaptcha();
+  }
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Navigation />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <Switch>
+            <Route path="/" exact component={() => <Home />} />
+            <Route
+              path="/middleware"
+              exact
+              component={() => <Middleware key={eventKeys.UNREAL} />}
+            />
+            <Route path="/games" exact component={() => <Games />} />
+            <Route path="/support" exact component={() => <Support />} />
+            <Route path="/about" exact component={() => <About />} />
+          </Switch>
+          <Footer />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
